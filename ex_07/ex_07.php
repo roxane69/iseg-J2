702975b2e2655d53c9801c63ef5d7ce45944219f
Orieux <orieux@MacBook-Air-de-Orieux.local>
2021-06-29 17:34:53 +0200

    exo

--- ex_07/ex_07.php
+++ ex_07/ex_07.php
@@ -1,5 +1,11 @@
 <?php
-for ($i=0; $i<3; $i++) {
- echo "woof";
- };
+function  get_angry_dog(int $nbr)
+{
+$NAME= "woof";
+for ($i=0; $i < $nbr; $i++) {
+   $string = $string . "woof";
+ }
+ 
+ return $string;
+}
 ?>
---
+++ ex_07/index.php
@@ -0,0 +1,4 @@
+>?php
+require'ex_07.php';
+echo ( get_angry_dog(3));
+?>
